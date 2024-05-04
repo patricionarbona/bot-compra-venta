d511bbc79df052b5dd3b1e9585fb7edf0bd3f84f
patricionarbona <patricioinarbona@gmail.com>
2024-05-04 13:40:36 +0200

    agregada funcion, seleccionar roi

--- bot.py
+++ bot.py
@@ -5,3 +5,10 @@ import pyautogui
 def captura_pantalla(nombre_archivo):
     captura = pyautogui.screenshot()
     captura.save(nombre_archivo)
+    return nombre_archivo
+
+def seleccionar_roi(imagen):
+    img = cv2.imread(imagen)
+    roi = cv2.selectROI("Selecciona la ROI", img)
+    cv2.destroyAllWindows()
+    return roi
